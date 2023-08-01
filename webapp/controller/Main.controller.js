40251feee2544d9a1468889a60827a8d42ee4369
Juan Pablo Fernandez Tubello <juanfernatubello@gmail.com>
2023-08-01 20:57:49 +0000

    added token verification UI

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, JSONModel) {
+    function (Controller, JSONModel, MT) {
         "use strict";
 
         return Controller.extend("fiori.cap.fioriwithcapp.controller.Main", {
@@ -17,9 +18,10 @@ sap.ui.define([
                         const response = res.results[0];
                         let QRimage = response.image.substring(1);
                         var oModel = new JSONModel({
-                            HTML: `<img src=${QRimage}>`
+                            HTML: `<img src=${QRimage}>`,
+                            secret: response.secret
                         });
-                        that.getView().setModel(oModel,"QRModel")
+                        that.getView().setModel(oModel, "QRModel")
                         that.byId("idHTMLContent").setContent(`<img src="${QRimage}">`);
                     },
                     error: function (err) {
@@ -27,6 +29,50 @@ sap.ui.define([
                     }
 
                 });
+            },
+            onPress: function () {
+                const inputToken = this.byId("inputText")
+                const token = inputToken.getValue()
+                const oService = this.getOwnerComponent().getModel("mainService")
+                oService.read(`/verifyToken(token='${token}')`, {
+                    success: function (res) {
+                        if (res.isValid) {
+                            MT.show("✔️ Token valido ✔️")
+                            inputToken.setValueState("Success")
+                        } else {
+                            MT.show("❌ Token invalido ❌")
+                            inputToken.setValueState("Error")
+                        }
+
+                    },
+                    error: function (err) {
+                        console.log(err)
+                    }
+
+                });
+
+            },
+            onChange: function (oEvent) {
+                const oState = oEvent.getParameters("state")
+                const inputToken = this.byId("inputText")
+                const labelToken = this.byId("l-token")
+                const qrCode = this.byId("idHTMLContent")
+                const button = this.byId("b-btn-token")
+                const textoQR = this.byId("l-qr-desc")
+                if (oState.state) {
+                    inputToken.setVisible(false)
+                    labelToken.setVisible(false)
+                    qrCode.setVisible(true)
+                    textoQR.setVisible(true)
+                    button.setVisible(false)
+                } else {
+                    inputToken.setVisible(true)
+                    labelToken.setVisible(true)
+                    qrCode.setVisible(false)
+                    textoQR.setVisible(false)
+                    button.setVisible(true)
+                }
+
             }
         });
     });
